087c40667547a3d5e331bcd4e5346f84dc8b192a
Nick DeSteffen <nick.desteffen@gmail.com>
2013-09-03 14:33:06 -0500

    Initial import

---
+++ README.md
@@ -0,0 +1,7 @@
+W3C Markup Validator Bootstrap
+==============
+
+Bash script to install the W3C Markup Validator on Scientific Linux 6
+  
+**To install:**  
+`curl -L https://raw.github.com/watermelonexpress/bootstrap-w3c-validtor/master/bootstrap.sh | sudo bash`  
---
+++ bootstrap.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+yum groupinstall -y "Development Tools"
+yum install -y kernel-devel kernel-headers gcc libxml2 libxml2-devel zlib-devel opensp-devel perl-CPAN
+
+mkdir -p ~/src
+cd ~/src
+wget http://github.com/downloads/petdance/tidyp/tidyp-1.04.tar.gz
+tar -xzf tidyp-1.04.tar.gz
+cd tidyp-1.04
+./configure
+make
+make install
+ldconfig
+
+perl -MCPAN -e "install Config::General"
+perl -MCPAN -e "install Bundle::W3C::Validator"
+
+cd ~/src
+wget http://validator.w3.org/validator.tar.gz http://validator.w3.org/sgml-lib.tar.gz
+tar -xzf validator.tar.gz
+tar -xzf sgml-lib.tar.gz
+
+mkdir -p /usr/local/validator
+mv validator-1.3/httpd/cgi-bin /usr/local/validator
+mv validator-1.3/{htdocs,share,httpd} /usr/local/validator
+mv validator-1.1/htdocs/sgml-lib /usr/local/validator/htdocs/
+
+mkdir -p /etc/w3c
+cp /usr/local/validator/htdocs/config/* /etc/w3c/
+
